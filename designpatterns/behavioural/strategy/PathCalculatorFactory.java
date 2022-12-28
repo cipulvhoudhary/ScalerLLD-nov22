16a6601832fbd0ded8e3856fae3bb2275a265b67
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-12-28 14:15:46 +0530

    Update PathCalculatorFactory.java

--- designpatterns/behavioural/strategy/PathCalculatorFactory.java
+++ designpatterns/behavioural/strategy/PathCalculatorFactory.java
@@ -1,7 +1,8 @@
 package com.designpatterns.behavioural.strategy;
 
 public class PathCalculatorFactory {
-
+    
+    // This does not violates srp and ocp, as factory is a part of business logic
     public static PathCalculatorStrategy getPathFromMode(String mode) {
         if(mode.equalsIgnoreCase("Car")) {
             return new CarPathCalculatorStrategy();
