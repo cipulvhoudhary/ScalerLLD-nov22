ba3ff8adeed814319147487d48f03f3d9ec73b68
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-12-28 14:14:35 +0530

    Add files via upload

---
+++ designpatterns/behavioural/Main.java
@@ -0,0 +1,4 @@
+package com.designpatterns.behavioural;
+
+public class Main {
+}
---
+++ designpatterns/behavioural/strategy/PathCalculatorStrategy.java
@@ -0,0 +1,7 @@
+package com.designpatterns.behavioural.strategy;
+
+public interface PathCalculatorStrategy {
+
+    void findPath(String from, String to);
+
+}
